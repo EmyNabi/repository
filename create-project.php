508df9ef1a235a7adfbdd66b88765cc095a968e1
EYN <nabildav@g.ucla.edu>
2025-08-03 15:46:38 -0700

    feat: streamline project layout editing

--- create-project.php
+++ create-project.php
@@ -7,12 +7,35 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo json_encode(['error' => 'invalid key']);
         exit;
     }
+
     $dir = "Projects/$key";
     if (!is_dir($dir)) {
-        mkdir("$dir/Images", 0777, true);
-        $template = file_get_contents('project-template.html');
+        if (!mkdir("$dir/Images", 0777, true)) {
+            http_response_code(500);
+            echo json_encode(['error' => 'failed to create project directory']);
+            exit;
+        }
+        $template = @file_get_contents('project-template.html');
+        if ($template === false) {
+            http_response_code(500);
+            echo json_encode(['error' => 'missing project template']);
+            exit;
+        }
         $html = str_replace('{{PROJECT_KEY}}', $key, $template);
-        file_put_contents("$dir/$key.html", $html);
+        if (file_put_contents("$dir/$key.html", $html) === false) {
+            http_response_code(500);
+            echo json_encode(['error' => 'failed to write project file']);
+            exit;
+        }
+    }
+
+    $projectsFile = 'all-projects.json';
+    $projects = file_exists($projectsFile)
+        ? json_decode(file_get_contents($projectsFile), true)
+        : [];
+    if (!isset($projects[$key])) {
+        $projects[$key] = [];
+        file_put_contents($projectsFile, json_encode($projects, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
     }
     echo json_encode(['status' => 'ok']);
 } else {
